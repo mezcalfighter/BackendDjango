d6f2ee364b04df0aab9461f39347f601eceee6a3
Emanuel Camarena <emanuel.ortega@hotmail.com>
2024-07-03 12:32:20 -0600

    Added Functional programming, pending classes

--- ZTM/PythonMaster/Functional Programming/lambda.py
+++ ZTM/PythonMaster/Functional Programming/lambda.py
@@ -1,3 +1,15 @@
 # Lambda Expresion - One time function expresions but you don't need anymore
 #lambda param: action(param)
 print(list(map(lambda item: item*2, [1,2,3,4,5])))
+
+# Exercise: Lambda Expresions
+# Square a list
+my_list = [5,4,3]
+my_list.sort()
+my_list = list(map(lambda num: num**2,my_list))
+print(my_list)
+
+# List sorting 
+a = [(0,2),(4,3),(9,9),(10,-1)]
+a.sort(key=lambda x:x[1])
+print(a)
