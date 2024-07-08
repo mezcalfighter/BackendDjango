4607d49d0c66e65d831958ec68df5f34f888a437
Emanuel Camarena <emanuel.ortega@hotmail.com>
2024-07-08 00:18:59 -0600

    Skipped automation, maybe in the future

--- ZTM/PythonMaster/WebPractice-Flask/server.py
+++ ZTM/PythonMaster/WebPractice-Flask/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,14 +7,20 @@ def my_home():
 
 @app.route("/<string:page_name>")
 def render_page(page_name):
-    print(f"{page_name}.html")
     return render_template(f"{page_name}")
+
+def write_to_file(data):
+    with open("database.txt",mode="a") as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f"\n{email},{subject},{message}")
 
 @app.route("/submit_form",methods=["POST","GET"])
 def submit_form():
     if request.method == "POST":
         data = request.form.to_dict()
-        print(data)
-        return redirect("./templates/thankyou.html")
+        write_to_file(data)
+        return redirect("thankyou.html")
     else:
         return "Something went wrong. Try Again"
